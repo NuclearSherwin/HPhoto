93c24b181028690283de9b6c46ba6c2c7202a055
paika2060 <paika2060@gmail.com>
2022-08-22 14:50:42 +0700

    init project

---
+++ HPhoto/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HPhoto
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Rating { get; set; } = string.Empty;
+
+
+    }
+}
